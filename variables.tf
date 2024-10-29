1cd433ed86262fe81fd0c9cb86c2fa16f3c09548
vedant-8680 <146105565+vedant-8680@users.noreply.github.com>
2024-10-30 02:48:38 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,25 +4,25 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "ap-south-2"
+  default     = "ap-south-1"
 }
 
 variable "cluster_name" {
   description = "Cluster name"
   type = string
-  default = "deploy-eks-test-11"
+  default = "deploy-eks-test-12"
 }
 
 variable "instance_type" {
   description = "Instance Type"
   type = string
-  default = "t3a.xlarge"
+  default = "t3a.large"
 }
 
 variable "min_size" {
   description = "Min number of nodes"
   type = number
-  default = 1
+  default = 2
 }
 
 variable "max_size" {
